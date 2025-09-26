af0adb6c4499c9c511aae3eae706e63ed9e0bec8
Joel <Joel.vargasmedal@gmail.com>
2025-09-26 19:16:34 +0200

    activities day one

---
+++ Day1/Algorithm1.php
@@ -0,0 +1,16 @@
+
+<?php
+
+function sum($Number1, $Number2){
+
+    return $Number1 == $Number2 ? ($Number1 + $Number2)*3 : $Number1 + $Number2;
+}
+
+echo sum(1,2). "\n";
+echo sum(3,2). "\n";
+echo sum(2,2);
+
+
+?>
+
+
---
+++ Day1/ForLoop1.php
@@ -0,0 +1,12 @@
+<?php
+
+$numbers = "";
+
+for ($x=1; $x <=10; $x++){
+   
+    $numbers = $numbers + "-";
+  
+}
+
+echo $numbers;
+?>
