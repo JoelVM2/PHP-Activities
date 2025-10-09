e4325c8e7b1506fabd06fef4d80a10e0e001350d
Joel <Joel.vargasmedal@gmail.com>
2025-10-09 20:46:07 +0200

    activities day 9

--- Day8/242matematicas.php
+++ Day8/242matematicas.php
@@ -12,20 +12,21 @@ function digitN(int $num,int $pos):int{
 
 function lastDigit(int $num,int $cant):int{
     
-    $last = substr($num, 0, $cant);
+    $last = substr($num, 0, strlen($num) - $cant);
     return $last;
 }
 
 function firstDigit(int $num,int $cant):int{
-    $last = substr($num, 0, $cant);
+    $last = substr($num, $cant);
     return $last;
 }
 
-echo digits(123445);
-echo digitN(1234,3);
-
-// terminar las dos funciones, lastdigit es funcional first digit no 
-echo lastDigit(1234567,-2);
-echo firstDigit(1234567,2);
+echo digits(123445) ;
+echo "---";
+echo digitN(1234,3) ;
+echo "---";
+echo lastDigit(1234567,2) ;
+echo "---";
+echo firstDigit(1234567,2) ;
 
 ?>
