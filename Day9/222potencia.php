2c9f03037cac9b59e8891c2e2e75d18116f957f4
Joel <Joel.vargasmedal@gmail.com>
2025-10-10 19:29:36 +0200

    Activities medrano day 10

--- Day9/222potencia.php
+++ Day9/222potencia.php
@@ -1,5 +1,13 @@
-<?php
+<?php 
 
+function potencia($base, $exponente){ 
+    $aaa = 1;
+    for ($i=$exponente; $i > 0 ; $i--) { 
+        $aaa = $aaa * $base;
+    }
+    return $aaa;
+}
 
+echo potencia(2,4);
 
 ?>
